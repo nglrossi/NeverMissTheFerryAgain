f1d4494102d809b80a5920525378efa03c1f0931
nglrossi <nglrossi@gmail.com>
2013-07-04 01:59:20 +1000

    Timer implemented: The clock is now ticking, refreshing the countdown every second string output updated:  the countdown is shown as hh:mm:ss if hh>0, else just mm:ss

--- NeverMissTheFerryAgain/NeverMissTheFerryAgain/src/com/nglrossi/mysecondapp/FerryTime.java
+++ NeverMissTheFerryAgain/NeverMissTheFerryAgain/src/com/nglrossi/mysecondapp/FerryTime.java
@@ -403,46 +403,53 @@ public class FerryTime {
 			ferryTime = getNextFerryToCircularQuayByTime(whatTime);
 		else
 			ferryTime = getNextFerryToManly(whatTime);
-		
+
 		long hrs, min, sec;
 		long tmpsec = 0;
-		
+
 		// check if the ferry is on the same day or add hours
 		if (ferryTime.weekDay >= whatTime.weekDay) {
 			tmpsec = ferryTime.toSeconds(false) - whatTime.toSeconds(false);
 		} else {
-			// I assume the Ferry is on the next day, but if new featureas are added to the app, I might have to review this method for a ferry in the future
-			tmpsec = ( ferryTime.toSeconds(false) + (60*60*24*7) ) - whatTime.toSeconds(false);			
+			// I assume the Ferry is on the next day, but if new featureas are
+			// added to the app, I might have to review this method for a ferry
+			// in the future
+			tmpsec = (ferryTime.toSeconds(false) + (60 * 60 * 24 * 7))
+					- whatTime.toSeconds(false);
 		}
-		
+
 		// convert back to hh:mm:ss
 		hrs = tmpsec / 3600;
 		min = (tmpsec % 3600) / 60;
-		sec = tmpsec % 60;		
-		
-		// string 
+		sec = tmpsec % 60;
+
+		// string
 		String str = "";
-		
-		// hours
-		if (hrs <10) str = str + "0";
-		str = str + Long.toString(hrs) + ":";
-		
+
+		// hours (show only if !=0)
+		if (hrs > 0) {
+			if (hrs < 10) str = str + "0";
+			str = str + Long.toString(hrs) + ":";
+		}
+
 		// minutes
-		if (min <10) str = str + "0";
-		str = str + Long.toString(min) + ":";		
+		if (min < 10)
+			str = str + "0";
+		str = str + Long.toString(min) + ":";
 
 		// seconds
-		if (sec <10) str = str + "0";
+		if (sec < 10)
+			str = str + "0";
 		str = str + Long.toString(sec);
-		
+
 		return str;
-		
-//		String debug = "";
-//		debug = debug + Integer.toString(theTime.hour) + ":";
-//		debug = debug + Integer.toString(theTime.minute) + ":";
-//		debug = debug + Integer.toString(theTime.second) + " - ";
-//		debug = debug + Integer.toString(theTime.weekDay);
-//		return debug;
+
+		// String debug = "";
+		// debug = debug + Integer.toString(theTime.hour) + ":";
+		// debug = debug + Integer.toString(theTime.minute) + ":";
+		// debug = debug + Integer.toString(theTime.second) + " - ";
+		// debug = debug + Integer.toString(theTime.weekDay);
+		// return debug;
 	}
 
 	public String countDownToCircularQuay(MyTime whatTime) {
--- NeverMissTheFerryAgain/NeverMissTheFerryAgain/src/com/nglrossi/mysecondapp/MainActivity.java
+++ NeverMissTheFerryAgain/NeverMissTheFerryAgain/src/com/nglrossi/mysecondapp/MainActivity.java
@@ -6,70 +6,81 @@ import com.nglrossi.MyTime;
 import android.app.Activity;
 //import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 //import android.view.Menu;
 //import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-    private static final FerryTime ferryTime = new FerryTime();
-    //public static final String EXTRA_MESSAGE = "com.nglrossi.mysecondapp.MESSAGE";
 
+	private static final FerryTime ferryTime = new FerryTime();
+	// public static final String EXTRA_MESSAGE =
+	// "com.nglrossi.mysecondapp.MESSAGE";
+
+	// used for calling back the same method over and over again
+	private Handler mHandler = new Handler();
+
+	// protected void keepRedrawing() {
+	private Runnable keepRedrawing = new Runnable() {
+		public void run() {
+
+			// Current time
+			MyTime now = new MyTime();
+			now.setToNow();
+
+			// get the time left for both trips
+			String nextFerryToCircularQuay = ferryTime
+					.countDownToCircularQuay(now);
+			String nextFerryToManly = ferryTime.countDownToManly(now);
+
+			// update widgets
+			TextView editViewMinutesToCircularQuay = (TextView) findViewById(R.id.countdown_to_circular_quay);
+			TextView editViewCountdownToManly = (TextView) findViewById(R.id.countdown_to_manly);
+			editViewMinutesToCircularQuay.setText(nextFerryToCircularQuay);
+			editViewCountdownToManly.setText(nextFerryToManly);
+
+			// do it again after 1 second
+			mHandler.removeCallbacks(keepRedrawing);
+			mHandler.postDelayed(keepRedrawing, 1000);
+		}
+
+	};
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 	}
-        
+
 	@Override
-    protected void onResume() {
-        super.onResume();
-        
-        // Current time
-        MyTime now = new MyTime();
-        now.setToNow();
-       
-        
-        // *********************************************
-        // Trip to Circular Quay
-        // *********************************************
-        
-        // find when it's the next ferry
-//        FerryTime ferryTime = new FerryTime();
-        String nextFerryToCircularQuay = ferryTime.countDownToCircularQuay(now);
-        String nextFerryToManly = ferryTime.countDownToManly(now);
-        
-        // update widgets
-        TextView editViewMinutesToCircularQuay = (TextView) findViewById(R.id.countdown_to_circular_quay);
-        editViewMinutesToCircularQuay.setText(nextFerryToCircularQuay);
-        
-        
-        
-        // *********************************************
-        // Trip to Manly
-        // *********************************************
-        TextView editViewCountdownToManly = (TextView) findViewById(R.id.countdown_to_manly);
-        editViewCountdownToManly.setText(nextFerryToManly);
-    }
+	protected void onResume() {
+		super.onResume();
+		mHandler.removeCallbacks(keepRedrawing);
+		mHandler.postDelayed(keepRedrawing, 100);
+	}
 
+	@Override
+	protected void onPause() {		
+		super.onPause();
+		mHandler.removeCallbacks(keepRedrawing);		
+	}
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);        
-//        return true;
-//    }
-    
-//    /** Called when the user clicks the Send button */
-//    public void sendMessage(View view) {
-//        // Do something in response to button
-//    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-//    	//EditText editText = (EditText) findViewById(R.id.edit_message);
-//    	//String message = editText.getText().toString();
-//    	//intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//
-//    }
-    
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.main, menu);
+	// return true;
+	// }
+
+	// /** Called when the user clicks the Send button */
+	// public void sendMessage(View view) {
+	// // Do something in response to button
+	// Intent intent = new Intent(this, DisplayMessageActivity.class);
+	// //EditText editText = (EditText) findViewById(R.id.edit_message);
+	// //String message = editText.getText().toString();
+	// //intent.putExtra(EXTRA_MESSAGE, message);
+	// startActivity(intent);
+	//
+	// }
+
 }
